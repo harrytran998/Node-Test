80021b97c4d50404f41d74e54b3048d959d44ccb
Loser Teach <jetaimefrc@gmail.com>
2018-08-29 00:31:23 +0700

    Test EsLint

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,13 +6,16 @@ const expect = require('expect');
 it('Should add two number', () => {
     let res = utils.add(33, 22);
     expect(res).toBe(40).toBeA(50);
-});
+})
 
-it('XXXX', () => {
+iat('XXXX', () => {
+    expect.
     expect({
         name: 'Nhat',
         age: 24
     }).toExclude({
         age:23
     })
-});
+})
+
+
